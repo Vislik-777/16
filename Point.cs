9914574e83dec72abdfcd79cd9ec2b8c738879e1
данил <skillip.ksss@mail.ru>
2025-11-11 12:31:22 +0300

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    using System;
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        // Перегрузка оператора сложения
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        // Перегрузка оператора вычитания
+        public static Point operator -(Point p1, Point p2)
+        {
+            return new Point(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        // Переопределение ToString()
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    class Program
+    {
+        static void Main()
+        {
+            var p1 = new Point(1, 2);
+            var p2 = new Point(3, 4);
+            Console.WriteLine(p1 + p2); // (4, 6)
+            Console.WriteLine(p2 - p1); // (2, 2)
+        }
+    }
+}
